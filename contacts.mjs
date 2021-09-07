f51b436ead2a00747e413db345d1446abf3b812d
Dmitry Karas <dmitrythekaras@gmail.com>
2021-09-07 16:17:23 +0300

    added error catching

--- contacts.mjs
+++ contacts.mjs
@@ -5,61 +5,84 @@ import { nanoid } from "nanoid";
 const contactsPath = path.resolve("./db/contacts.json");
 
 const readContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 async function listContacts() {
-  const contacts = await readContacts();
+  try {
+    const contacts = await readContacts();
 
-  console.table(contacts);
+    console.table(contacts);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getContactById(contactId) {
-  const contacts = await readContacts();
+  try {
+    const contacts = await readContacts();
 
-  const contact = contacts.find(
-    (contact) => String(contact.id) === String(contactId)
-  );
+    const contact = contacts.find(
+      (contact) => String(contact.id) === String(contactId)
+    );
 
-  console.table(contact);
+    if (!contact) throw new Error("Contact not found");
+
+    console.table(contact);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function removeContact(contactId) {
-  const contacts = await readContacts();
+  try {
+    const contacts = await readContacts();
+    const contact = await getContactById(contactId);
 
-  console.log("Removing a contact...");
+    if (!contact) return;
 
-  await getContactById(contactId);
+    console.log("Removing a contact...");
 
-  await fs.writeFile(
-    contactsPath,
-    JSON.stringify(
-      contacts.filter((contact) => String(contact.id) !== String(contactId)),
-      null,
-      2
-    )
-  );
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(
+        contacts.filter((contact) => String(contact.id) !== String(contactId)),
+        null,
+        2
+      )
+    );
 
-  console.table(await readContacts());
-  console.log("The contact has been successfully deleted");
+    console.table(await readContacts());
+    console.log("The contact has been successfully deleted");
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await readContacts();
-  const id = nanoid();
-  const contact = { id, name, email, phone };
+  try {
+    const contacts = await readContacts();
+    const id = nanoid();
+    const contact = { id, name, email, phone };
 
-  console.log("Adding a contact...");
-  console.table(contact);
+    console.log("Adding a contact...");
+    console.table(contact);
 
-  await fs.writeFile(
-    contactsPath,
-    JSON.stringify([...contacts, contact], null, 2)
-  );
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify([...contacts, contact], null, 2)
+    );
 
-  console.table(await readContacts());
-  console.log("The contact has been successfully added");
+    console.table(await readContacts());
+    console.log("The contact has been successfully added");
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export default { listContacts, getContactById, removeContact, addContact };
